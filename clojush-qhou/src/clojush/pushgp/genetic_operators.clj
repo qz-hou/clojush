059c2f3a5bc3fee95f75de94b5b1a4b48362f5db
qz-hou <61359588+qz-hou@users.noreply.github.com>
2021-06-22 21:14:26 +0800

    Add files via upload

--- clojush-qhou/src/clojush/pushgp/genetic_operators.clj
+++ clojush-qhou/src/clojush/pushgp/genetic_operators.clj
@@ -633,42 +633,6 @@ given by uniform-deletion-rate.
                                   (cons (:genome ind) (:ancestors ind))
                                   (:ancestors ind)))))
 
-
-(defn modified-uniform-addition-and-deletion
-  "Returns the individual after two passes of mutation. In the first pass, each element of
-  its genome may possibly be preceded or followed by a new gene. In the second pass, each
-  element of the genome may possibly be deleted. Probabilities are given by
-  uniform-addition-and-deletion-rate.
-  Works with Plushy genomes."
-  [ind {:keys [uniform-addition-and-deletion-rate add-instruction-from-other-rate maintain-ancestors atom-generators passed failed population]
-        :as argmap}]
-  (let [addition-rate (random-element-or-identity-if-not-a-collection uniform-addition-and-deletion-rate)
-        add-instruction-from-other-rate (random-element-or-identity-if-not-a-collection add-instruction-from-other-rate)
-        deletion-rate (if (zero? addition-rate)
-                        0
-                        (/ 1 (+ (/ 1 addition-rate) 1)))
-        after-addition (vec (apply concat
-                                   (mapv #(if (< (lrand) addition-rate)
-                                            (lshuffle [%
-                                                       (if (< (lrand) add-instruction-from-other-rate)
-                                                         (rand-nth (passed))
-                                                         ;(rand-nth (:genome (select population argmap)))
-                                                         (do (random-genome-gene atom-generators argmap)
-                                                           (if (map ((fn [coll ele] (some (fn [m] (= ele m)) coll))
-                                                                     [failed (random-genome-gene atom-generators argmap)])))
-                                                             (random-genome-gene atom-generators argmap)))])
-                                            [%])
-                                         (:genome ind))))
-        new-genome (vec (filter identity
-                                (mapv #(if (< (lrand) deletion-rate) nil %)
-                                      after-addition)))]
-    (make-individual :genome new-genome
-                     :history (:history ind)
-                     :grain-size (compute-grain-size new-genome ind argmap)
-                     :ancestors (if maintain-ancestors
-                                  (cons (:genome ind) (:ancestors ind))
-                                  (:ancestors ind)))))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; uniform combination
 
